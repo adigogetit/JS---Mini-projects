504eb0f2693d84d05f65ae1ba9902e755249adb2
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2025-11-29 21:37:28 +0530

    Calculator

---
+++ CALCULATOR/app.js
@@ -0,0 +1,29 @@
+
+// select all button together
+let buttons = document.querySelectorAll("button");
+
+// select input in which we want ot show the numbers
+let inp = document.querySelector("input");
+
+for(let btn of buttons){
+    btn.addEventListener("click",function(event){
+        let btnText = event.target.innerText;
+
+        if(btnText == 'C'){
+            inp.value='';
+        }
+        else if(btnText == '='){
+            try{
+                inp.value = eval(inp.value);
+            }
+            catch(event){
+                inp.value = "Noi bataunga"
+            }
+
+        }
+        else{
+            inp.value += btnText;
+        }
+
+    })
+}
