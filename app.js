50d26407a2660be80c7011b9c1f542c3611f28ef
AdityaSingh-my25 <adityasingh12ka36@gmail.com>
2025-11-30 18:16:15 +0530

    snake game done

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-// to select the canvas
-let canvas = document.querySelector("canvas");
-let ctx = canvas.getContext("2d");
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
 
 let cellSize = 50; //height & width ka kaam krega for each cell
 let boardHeight = 600;
 let boardWidth = 1000;
+
 let snakeCells = [ [0,0] ]; //2d array to store starting points of snake ka rectangle
+
 let direction  = 'right';
 
 let gameOver = false; //wall se touch hone ke baad hojaae true
+
+let foodCells = generateFood(); //becoz we need 2 values i.e x and y
+
+let score = 0;
 
 // baar baar repeat
 let intervalId = setInterval(function(){
@@ -29,14 +34,28 @@ document.addEventListener('keydown' , function(event){
 function draw(){
     if(gameOver === true){
         clearInterval(intervalId); //it accepts an id
+        ctx.fillStyle = 'red';
+        ctx.font = '50px monospace'
+        ctx.fillText('GAME OVER !!' , 350 , 300)
         return;
     }
 
+    // draw snake
     ctx.clearRect(0,0,boardWidth,boardHeight);
     for(let cell of snakeCells){
         ctx.fillStyle = 'yellow';
         ctx.fillRect(cell[0] , cell[1] , cellSize , cellSize);
+        ctx.strokeRect(cell[0] , cell[1] , cellSize , cellSize);
     }
+
+    // draw food
+    ctx.fillStyle = 'green'
+    ctx.fillRect(foodCells[0] , foodCells[1] , cellSize , cellSize );
+
+    // draw score
+    ctx.font = '24px monospace'
+    ctx.fillText(`Score: ${score}` , 20 , 25);
+
 }
 
 
@@ -54,25 +73,53 @@ function update(){
     if(direction === 'right'){
          newHeadX = headX + cellSize;
          newHeadY = headY;
-         if(newHeadX === boardWidth){gameOver = true}
+         if(newHeadX === boardWidth || khagyaKhudko(newHeadX,newHeadY) ){gameOver = true}
     }
     else if(direction === 'left'){
          newHeadX = headX - cellSize;
          newHeadY = headY;
-         if(newHeadX < 0){gameOver = true}
+         if(newHeadX < 0 || khagyaKhudko(newHeadX,newHeadY) ){gameOver = true}
     }
     else if(direction === 'up'){
          newHeadX = headX;
          newHeadY = headY - cellSize;
-         if( newHeadY < 0 ){gameOver = true}
+         if( newHeadY < 0 || khagyaKhudko(newHeadX,newHeadY) ){gameOver = true}
     }
     else{
          newHeadX = headX;
          newHeadY = headY + cellSize;
-         if(newHeadY === boardHeight){gameOver = true}
+         if(newHeadY === boardHeight || khagyaKhudko(newHeadX,newHeadY) ){gameOver = true}
     }
 
 
     snakeCells.push([newHeadX , newHeadY]);
-    snakeCells.shift();
+
+    // when snake touch the food then shift from last will stop for oe time that eill led to inc in nake size
+    if(newHeadX === foodCells[0] && newHeadY === foodCells[1] ){
+        foodCells = generateFood();
+        score += 1;
+    }
+    else{
+        snakeCells.shift();
+    }
+
+}
+
+
+function generateFood(){
+    return [
+        Math.round((Math.random()*(boardWidth - cellSize)) / cellSize) * cellSize,
+        Math.round((Math.random()*(boardHeight - cellSize)) / cellSize) * cellSize,
+    ]
+}
+
+
+function khagyaKhudko(newHeadX , newHeadY){
+    // loop
+    for(let item of snakeCells){
+        if(item[0] === newHeadX && item[1] === newHeadY){
+            return true;
+        }
+    }
+    return false;
 }
